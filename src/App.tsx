531e6b2bb533e57503c58e8d45302ce912ac1981
Alessio Ilardi <alessio.ilardi@gmail.com>
2023-04-24 17:48:09 +0200

    chore: move components to new Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import Text from './components/Text';
-import Button from './components/Button';
+import Home from './pages/Home';
 
 function App(): JSX.Element {
   return (
     <div className="App">
-      <Text variant="body1" color="">
-        Questo è un testo
-      </Text>
-      <Button text="Questo è un bottone" />
+      <Home />
     </div>
   );
 }
