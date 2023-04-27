dd45095d6cc0b3a28ea6b621ca11d7fe9cec8363
JacopoMarini <marinijacopo96@gmail.com>
2023-04-27 09:41:59 +0200

    minfix

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
-import { Home as HomeIcon } from '@mui/icons-material';
 import Navbar from '../../components/Navbar';
-import Button from '../../components/Button';
 import Sidebar from '../../components/Sidebar';
+import { DataGrid } from '../../components/DataGrid';
+import { columns, rows } from './mock';
+
+
 
 function Home() {
   return (
     <>
-      <Navbar title="Dashboard" />
       <Sidebar></Sidebar>
+      <>
+      <Navbar title="Dashboard" />
+      <DataGrid rows={rows} columns={columns} checkboxSelection></DataGrid>
+      </>
     </>
   );
 }
