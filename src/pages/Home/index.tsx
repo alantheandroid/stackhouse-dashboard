264e89658d397814ac3fefe02b94064d56f0740b
JacopoMarini <marinijacopo96@gmail.com>
2023-05-02 17:05:53 +0200

    remove unused input

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,7 @@ import { columns, rows } from './mock';
 import { Searchbar } from '../../components/Searchbar';
 import { FormCheckbox } from '../../components/FormComponents/FormCheckbox';
 import { FormInput } from '../../components/FormComponents/FormInput';
-import { FormInputNumber } from '../../components/FormComponents/FormInputNumber';
+
 
 function Home() {
   return (
@@ -13,7 +13,6 @@ function Home() {
     <DataGrid rows={rows} columns={columns} checkboxSelection hidefooter/>
     <FormCheckbox label='prova' onChange={() => {}}/>
     <FormInput label='inpit' onChange={() => {}} />
-    <FormInputNumber label='number' onChange={() => {}} />
   </>  
   );
 }
