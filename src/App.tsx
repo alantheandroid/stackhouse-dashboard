48ed65e69f132f0820010689cf8af2a383071e73
Alessio Ilardi <alessio.ilardi@gmail.com>
2023-05-04 15:07:13 +0200

    📦 NEW: add form component test route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import Home from './pages/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import { FormBuilder } from './pages/FormBuilder';
+import Input from './components/Input';
+import Select from './components/Select';
+import Checkbox from './components/Checkbox';
+import Switch from './components/Switch';
+import { FormGroup } from '@mui/material';
 
 function App(): JSX.Element {
   return (
@@ -12,6 +17,17 @@ function App(): JSX.Element {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="form-builder" element={<FormBuilder />} />
+            <Route
+              path="form-components-test"
+              element={
+                <FormGroup>
+                  <Input label="Input" />
+                  <Select options={['Test']} />
+                  <Checkbox label="Checkbox" />
+                  <Switch label="Switch" />
+                </FormGroup>
+              }
+            />
           </Route>
         </Routes>
       </BrowserRouter>
