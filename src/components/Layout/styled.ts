d8484ccfbbb0c731726082d21c8eba970b10c4e7
Alessio Ilardi <alessio.ilardi@gmail.com>
2023-05-04 15:11:15 +0200

    👌 IMPROVE: add padding-right to StyledContainer in Layout component

--- src/components/Layout/styled.ts
+++ src/components/Layout/styled.ts
@@ -1,17 +1,18 @@
-import styled from "@emotion/styled";
-import { secondary } from "../../styles/themeColors";
+import styled from '@emotion/styled';
+import { secondary } from '../../styles/themeColors';
 
 export const StyledLayout = styled.div`
-    display: flex;
-    background-color: ${() => secondary[200]};
-    gap: 3rem;
-`
+  display: flex;
+  background-color: ${() => secondary[200]};
+  gap: 3rem;
+`;
 
 export const StyledContainer = styled.div`
-display: flex;
-flex-direction: column;
-flex: 1;
-max-width: 1240px;
-margin: 0 auto;
-gap: 1.5rem;
-`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  max-width: 1240px;
+  margin: 0 auto;
+  gap: 1.5rem;
+  padding-right: 1rem;
+`;
