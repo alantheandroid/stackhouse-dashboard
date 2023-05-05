a74e50af4b49bbb96c1e085cb5ca165f332d296b
sergiogalici <galici.sergio@gmail.com>
2023-05-05 15:45:38 +0200

    Quill TextArea works

--- src/components/FormComponents/FormTextArea/index.tsx
+++ src/components/FormComponents/FormTextArea/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box } from '@mui/material';
 import Text from '../../Text';
 import { OptionsType } from '../../FormFactory/formModel';
 import { FormElementProps } from '../formElementsProps';
 import { useDispatch, useSelector } from 'react-redux';
 import { makeSelectElementValueByElementId } from '../../../features/formBuilder/selector';
-import { formBuilderActions } from '../../../features/formBuilder/slice';
+import Quill from 'quill';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 export const FormTextArea = ({
   label,
@@ -19,18 +21,33 @@ export const FormTextArea = ({
     makeSelectElementValueByElementId(formId, elementId)
   );
 
-
   const handleDisabled = disabled ?? isDisabled;
+
+  const quillOptions = {
+    modules: {
+      toolbar: [
+        [{ header: [1, 2, 3, false] }],
+        ['bold', 'italic', 'underline', 'strike'],
+        [{ color: [] }, { background: [] }],
+        [{ list: 'ordered' }, { list: 'bullet' }],
+        [{ align: [] }],
+        ['link', 'image', 'video'],
+        ['clean'],
+      ],
+    },
+    theme: 'snow',
+  };
+
   return (
     <Box flexDirection="column" gap="4px">
       <Box justifyContent="flex-start">
         <Text>{label}</Text>
       </Box>
-      <textarea
+      <ReactQuill
+        theme="snow"
         value={value as string}
-        placeholder={label}
-        disabled={handleDisabled}
         onChange={(e) => onChange(elementId, e)}
+        modules={quillOptions.modules}
       />
     </Box>
   );
